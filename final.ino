b1207215cfb2512a552dfa90f53b3d500fa0c2f3
Thiha <thiha1122000@gmail.com>
2026-06-12 22:13:49 +0630

    Update final.ino

--- final.ino
+++ final.ino
@@ -1,46 +1,45 @@
 #include <SPI.h>
 #include <MFRC522.h>
+#include <avr/wdt.h>
 
+// ============================
+// RFID
+// ============================
 #define SS_PIN 10
-#define RST_PIN 9
-#define BUZZER_PIN 5
+#define RST_PIN 8
 
-MFRC522 rfid(SS_PIN, RST_PIN);
-
-// ============================
-// 74HC595 PINS
-// ============================
-const int DATA_PIN  = 2;
-const int LATCH_PIN = 3;
-const int CLOCK_PIN = 4;
+MFRC522 mfrc522(SS_PIN, RST_PIN);
 
 // ============================
-// SYSTEM MODES
+// BUZZER (optional if already used, safe to keep)
 // ============================
-#define MODE_NORMAL  0
-#define MODE_FUNCTION 1
-#define MODE_ADMIN    2
-
-byte systemMode = MODE_NORMAL;
+#define BUZZER_PIN 6
 
 // ============================
-// RELAY STATE (ACTIVE LOW)
+// RELAYS
 // ============================
-uint16_t outputs = 0xFFFF;
+const int groundRelay = 22;
+
+const int floorRelay[12] = {
+  23, 24, 25, 26, 27, 28,
+  29, 30, 31, 32, 33, 34
+};
 
 // ============================
-// FLOOR TIMER
+// RELAY TYPES
 // ============================
-unsigned long floorTimer = 0;
-byte activeFloor = 0;
-bool floorActive = false;
+const bool FLOOR_ACTIVE_LOW = true;
+const bool GROUND_ACTIVE_HIGH = true;
 
 // ============================
-// SPECIAL CARDS
+// STATE
 // ============================
-byte installCard[4] = {0x7B, 0x44, 0x29, 0xD5}; // FUNCTION CARD
+const unsigned long ACCESS_TIME = 5000;
+unsigned long accessStart = 0;
+bool accessActive = false;
 
-byte adminCard[4]   = {0xAA, 0xBB, 0xCC, 0xDD}; // CHANGE THIS!
+int activeFloor = -1;
+bool serviceMode = false;
 
 // ============================
 // SERVICE CARDS
@@ -53,47 +52,134 @@ byte serviceCards[][4] = {
 };
 
 // ============================
-// FLOOR DATABASE (unchanged)
+// FLOOR CARDS
 // ============================
-byte floor1[][4] = {{0xD7,0x6D,0x85,0xAD},{0x67,0x0B,0xD4,0xAD}};
-byte floor2[][4] = {{0x73,0xF2,0x8E,0x05},{0x79,0x3C,0x8F,0x05}};
-byte floor3[][4] = {{0x7A,0x5E,0x8E,0x05},{0x15,0xD6,0x8E,0x05}};
-byte floor4[][4] = {{0x17,0x70,0xA8,0xAD},{0x07,0xEE,0x67,0xAD}};
-byte floor5[][4] = {{0x04,0x58,0x8F,0x05},{0x04,0xEC,0x8E,0x05}};
-byte floor6[][4] = {{0xE3,0xCE,0xF5,0x27},{0x67,0xEB,0xB5,0xAD}};
-byte floor7[][4] = {{0x97,0xE5,0xBF,0xAD},{0xD7,0x8A,0x88,0xAD}};
-byte floor8[][4] = {{0xA7,0x88,0xD5,0xAD},{0x67,0x0D,0x5E,0xAD}};
-byte floor9[][4] = {{0xD7,0xF4,0x8F,0xAD},{0xE7,0x7D,0xA8,0xAD}};
-byte floor10[][4] = {{0x77,0x4D,0xCC,0xAD},{0x37,0x28,0xB7,0xAD}};
-byte floor11[][4] = {{0x77,0xAC,0xC8,0xAD},{0x27,0xC4,0x62,0xAD}};
-byte floor12[][4] = {{0x97,0x5B,0x7D,0xAD},{0xC7,0xC8,0x8C,0xAD}};
+byte floorCards[12][6][4] = {
+  {{0xD7,0x6D,0x85,0xAD},{0x67,0x0B,0xD4,0xAD},{0x47,0x40,0xC2,0xAD},{0xA7,0xF6,0x8F,0xAD},{0x57,0xC5,0xB2,0xAD},{0x07,0x22,0x8B,0xAD}},
+  {{0x73,0xF2,0x8E,0x05},{0x79,0x3C,0x8F,0x05},{0xEC,0x51,0x8E,0x05},{0x27,0xFE,0x89,0xAD},{0xE7,0x78,0xA8,0xAD},{0x67,0xC7,0x69,0xAD}},
+  {{0x7A,0x5E,0x8E,0x05},{0x15,0xD6,0x8E,0x05},{0x69,0xCB,0x8F,0x05},{0x6E,0xF2,0x8E,0x05},{0x91,0x2B,0x8F,0x05},{0x6B,0x82,0x8E,0x05}},
+  {{0x17,0x70,0xA8,0xAD},{0x07,0xEE,0x67,0xAD},{0x07,0xB3,0x9D,0xAD},{0x4B,0x14,0x8F,0x05},{0xD7,0x20,0x78,0xAD},{0xF7,0xBA,0xC4,0xAD}},
+  {{0x04,0x58,0x8F,0x05},{0x04,0xEC,0x8E,0x05},{0x7B,0x66,0x8E,0x05},{0x27,0x1B,0x69,0xAD},{0x87,0x24,0xC2,0xAD},{0xB4,0x80,0x8E,0x05}},
+  {{0xE3,0xCE,0xF5,0x27},{0x67,0xEB,0xB5,0xAD},{0x47,0xF3,0x6A,0xAD},{0xC7,0x69,0xA2,0xAD},{0x27,0x9D,0xB9,0xAD},{0x67,0x4C,0xB9,0xAD}},
+  {{0x97,0xE5,0xBF,0xAD},{0xD7,0x8A,0x88,0xAD},{0x87,0x1F,0xC5,0xAD},{0xA7,0x4F,0x82,0xAD},{0xC7,0xB9,0x94,0xAD},{0xE7,0xA9,0x58,0xAD}},
+  {{0xA7,0x88,0xD5,0xAD},{0x67,0x0D,0x5E,0xAD},{0x87,0x0D,0x72,0xAD},{0xD7,0xC6,0xCC,0xAD},{0x47,0xF7,0xC9,0xAD},{0x47,0x67,0xAC,0xAD}},
+  {{0xD7,0xF4,0x8F,0xAD},{0xE7,0x7D,0xA8,0xAD},{0x1E,0x95,0x6A,0x05},{0x49,0x4F,0x8E,0x05},{0xC6,0x00,0x71,0x05},{0x1D,0x47,0x8E,0x05}},
+  {{0x77,0x4D,0xCC,0xAD},{0x37,0x28,0xB7,0xAD},{0x17,0xB9,0xD0,0xAD},{0xF7,0x7A,0xB5,0xAD},{0x67,0xE5,0x8E,0x05},{0xB9,0x5E,0x8E,0x05}},
+  {{0x77,0xAC,0xC8,0xAD},{0x27,0xC4,0x62,0xAD},{0xC7,0x6A,0x8E,0xAD},{0xD7,0xC9,0x87,0xAD},{0xC7,0x80,0x89,0xAD},{0x17,0xF0,0xB2,0xAD}},
+  {{0x97,0x5B,0x7D,0xAD},{0xC7,0xC8,0x8C,0xAD},{0xF7,0xE4,0x8E,0xAD},{0xD7,0x41,0x59,0xAD},{0x17,0x72,0x57,0xAD},{0x77,0x12,0x56,0xAD}}
+};
 
 // ============================
-// UTIL
+// RELAY CONTROL
 // ============================
-bool compare(byte *a, byte *b, byte len) {
-  for (byte i = 0; i < len; i++) {
+void floorON(int pin) {
+  digitalWrite(pin, FLOOR_ACTIVE_LOW ? LOW : HIGH);
+}
+
+void floorOFF(int pin) {
+  digitalWrite(pin, FLOOR_ACTIVE_LOW ? HIGH : LOW);
+}
+
+void groundON() {
+  digitalWrite(groundRelay, GROUND_ACTIVE_HIGH ? HIGH : LOW);
+}
+
+void groundOFF() {
+  digitalWrite(groundRelay, GROUND_ACTIVE_HIGH ? LOW : HIGH);
+}
+
+// ============================
+// SAFE STATE
+// ============================
+void safeState() {
+
+  for (int i = 0; i < 12; i++) {
+    floorOFF(floorRelay[i]);
+  }
+
+  groundOFF();
+
+  accessActive = false;
+  serviceMode = false;
+  activeFloor = -1;
+
+  Serial.println("SAFE STATE");
+}
+
+// ============================
+// BUZZER
+// ============================
+void beep(int t) {
+  digitalWrite(BUZZER_PIN, HIGH);
+  delay(t);
+  digitalWrite(BUZZER_PIN, LOW);
+}
+
+void beepSuccess() { beep(100); }
+void beepService() { beep(100); delay(100); beep(100); }
+void beepError() { beep(500); }
+
+// ============================
+// UID MATCH
+// ============================
+bool matchUID(byte *a, byte *b) {
+  for (byte i = 0; i < 4; i++) {
     if (a[i] != b[i]) return false;
   }
   return true;
 }
 
 // ============================
+// FLOOR FIND
+// ============================
+int getFloor(byte *uid) {
+  for (int f = 0; f < 12; f++) {
+    for (int c = 0; c < 6; c++) {
+      if (matchUID(uid, floorCards[f][c])) return f;
+    }
+  }
+  return -1;
+}
+
+// ============================
+// SERVICE CHECK
+// ============================
+bool isService(byte *uid) {
+  for (int i = 0; i < 4; i++) {
+    if (matchUID(uid, serviceCards[i])) return true;
+  }
+  return false;
+}
+
+// ============================
 // SETUP
 // ============================
 void setup() {
-  Serial.begin(9600);
-  SPI.begin();
-  rfid.PCD_Init();
 
-  pinMode(DATA_PIN, OUTPUT);
-  pinMode(LATCH_PIN, OUTPUT);
-  pinMode(CLOCK_PIN, OUTPUT);
+  Serial.begin(9600);
+
   pinMode(BUZZER_PIN, OUTPUT);
 
-  updateShiftRegister();
+  for (int i = 0; i < 12; i++) {
+    pinMode(floorRelay[i], OUTPUT);
+  }
 
-  Serial.println("=== SYSTEM READY ===");
+  pinMode(groundRelay, OUTPUT);
+
+  safeState();
+
+  // WATCHDOG
+  wdt_enable(WDTO_2S);
+
+  // RC522 POWER STABILITY DELAY
+  delay(1000);
+  SPI.begin();
+  delay(200);
+
+  mfrc522.PCD_Init();
+  delay(100);
+
+  Serial.println("SYSTEM READY");
 }
 
 // ============================
@@ -101,141 +187,55 @@ void setup() {
 // ============================
 void loop() {
 
-  if (!rfid.PICC_IsNewCardPresent()) return;
-  if (!rfid.PICC_ReadCardSerial()) return;
+  wdt_reset();
 
-  byte *uid = rfid.uid.uidByte;
-  byte len = rfid.uid.size;
-
-  // ============================
-  // ADMIN CARD
-  // ============================
-  if (compare(uid, adminCard, len)) {
-    beep();
-    systemMode = MODE_ADMIN;
-    Serial.println("ADMIN MODE ACTIVATED");
-    rfid.PICC_HaltA();
-    return;
+  // periodic RFID recovery
+  static unsigned long rfidFix = 0;
+  if (millis() - rfidFix > 10000) {
+    mfrc522.PCD_Init();
+    rfidFix = millis();
   }
 
-  // ============================
-  // FUNCTION CARD TOGGLE
-  // ============================
-  if (compare(uid, installCard, len)) {
-    beep();
+  if (!mfrc522.PICC_IsNewCardPresent()) return;
+  if (!mfrc522.PICC_ReadCardSerial()) return;
 
-    if (systemMode == MODE_NORMAL) {
-      systemMode = MODE_FUNCTION;
-      Serial.println("FUNCTION MODE ON");
-    } else {
-      systemMode = MODE_NORMAL;
-      Serial.println("FUNCTION MODE OFF");
+  byte *uid = mfrc522.uid.uidByte;
+
+  int floor = getFloor(uid);
+
+  if (isService(uid)) {
+
+    beepService();
+    groundON();
+
+    for (int i = 0; i < 12; i++) {
+      floorON(floorRelay[i]);
     }
 
-    rfid.PICC_HaltA();
+    serviceMode = true;
+    accessActive = true;
+  }
+
+  else if (floor != -1) {
+
+    beepSuccess();
+
+    groundON();
+    floorON(floorRelay[floor]);
+
+    activeFloor = floor;
+    accessActive = true;
+  }
+
+  else {
+
+    beepError();
+    safeState();
     return;
   }
 
-  // ============================
-  // ADMIN MODE ACTION
-  // ============================
-  if (systemMode == MODE_ADMIN) {
-    Serial.println("ADMIN ACCESS → ALL FLOORS ON");
+  accessStart = millis();
 
-    outputs = 0x0000;
-    updateShiftRegister();
-
-    delay(3000);
-
-    outputs = 0xFFFF;
-    updateShiftRegister();
-
-    systemMode = MODE_NORMAL;
-
-    rfid.PICC_HaltA();
-    return;
-  }
-
-  // ============================
-  // NORMAL + FUNCTION MODE ACCESS
-  // ============================
-  if (systemMode == MODE_NORMAL || systemMode == MODE_FUNCTION) {
-
-    if (checkFloor(uid, floor1, 6, len)) granted(1);
-    else if (checkFloor(uid, floor2, 6, len)) granted(2);
-    else if (checkFloor(uid, floor3, 6, len)) granted(3);
-    else if (checkFloor(uid, floor4, 6, len)) granted(4);
-    else if (checkFloor(uid, floor5, 6, len)) granted(5);
-    else if (checkFloor(uid, floor6, 6, len)) granted(6);
-    else if (checkFloor(uid, floor7, 6, len)) granted(7);
-    else if (checkFloor(uid, floor8, 6, len)) granted(8);
-    else if (checkFloor(uid, floor9, 6, len)) granted(9);
-    else if (checkFloor(uid, floor10, 6, len)) granted(10);
-    else if (checkFloor(uid, floor11, 6, len)) granted(11);
-    else if (checkFloor(uid, floor12, 6, len)) granted(12);
-    else error();
-  }
-
-  rfid.PICC_HaltA();
-}
-
-// ============================
-// GRANTED
-// ============================
-void granted(byte floor) {
-
-  beep();
-
-  Serial.print("ACCESS GRANTED → FLOOR ");
-  Serial.println(floor);
-
-  outputs &= ~(1 << (floor - 1));
-  updateShiftRegister();
-
-  activeFloor = floor;
-  floorTimer = millis();
-  floorActive = true;
-}
-
-// ============================
-// ERROR
-// ============================
-void error() {
-  Serial.println("ACCESS DENIED");
-  errorBeep();
-}
-
-// ============================
-// FLOOR CHECK
-// ============================
-bool checkFloor(byte *uid, byte floor[][4], int size, byte len) {
-  for (int i = 0; i < size; i++) {
-    if (compare(uid, floor[i], len)) return true;
-  }
-  return false;
-}
-
-// ============================
-// SHIFT REGISTER
-// ============================
-void updateShiftRegister() {
-  digitalWrite(LATCH_PIN, LOW);
-  shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, highByte(outputs));
-  shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, lowByte(outputs));
-  digitalWrite(LATCH_PIN, HIGH);
-}
-
-// ============================
-// BEEP
-// ============================
-void beep() {
-  digitalWrite(BUZZER_PIN, HIGH);
-  delay(120);
-  digitalWrite(BUZZER_PIN, LOW);
-}
-
-void errorBeep() {
-  digitalWrite(BUZZER_PIN, HIGH);
-  delay(400);
-  digitalWrite(BUZZER_PIN, LOW);
+  mfrc522.PICC_HaltA();
+  mfrc522.PCD_StopCrypto1();
 }
